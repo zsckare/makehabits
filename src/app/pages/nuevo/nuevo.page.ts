171b9bfa4694b7be28c4979520034377db3c5916
Antonio Alvarez <tokyo.blue.tsubasa@gmail.com>
2021-06-10 07:53:49 -0500

    add bottonsheet

--- src/app/pages/nuevo/nuevo.page.ts
+++ src/app/pages/nuevo/nuevo.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DbService } from '../../services/db.service';
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-nuevo',
   templateUrl: './nuevo.page.html',
@@ -11,7 +12,7 @@ export class NuevoPage implements OnInit {
   Data: any[] = [];
   info: any;
     
-  constructor(private db: DbService,) { }
+  constructor(private db: DbService,public actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
     this.db.dbState().subscribe((res) => {
@@ -24,6 +25,38 @@ export class NuevoPage implements OnInit {
         })
       }
     }); 
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Edit',
+        icon: 'pencil',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, , {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 
 }
